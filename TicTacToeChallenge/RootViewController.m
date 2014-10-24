8723b9b8590bf3234a732d4cf8f52b5223c57a55
Andrew Liu <dieseliu321@gmail.com>
2014-10-23 20:56:48 -0700

    working on disable label

--- TicTacToeChallenge/RootViewController.m
+++ TicTacToeChallenge/RootViewController.m
@@ -29,89 +29,87 @@
     [super viewDidLoad];
 }
 
-- (void) findLabelUsingPoint: (CGPoint)point
+- (UILabel *)findLabelUsingPoint:(CGPoint)point
 {
-//define CGlabel
-
-
-
-
+    if (CGRectContainsPoint(self.labelOne.frame, point))
+        {
+            return self.labelOne;
+        }
+    else
+        if (CGRectContainsPoint(self.labelTwo.frame, point))
+        {
+            return self.labelTwo;
+        }
+    else
+        if (CGRectContainsPoint(self.labelThree.frame, point))
+        {
+            return self.labelThree;
+        }
+    else
+        if (CGRectContainsPoint(self.labelFour.frame, point))
+        {
+            return self.labelFour;
+        }
+    else
+        if (CGRectContainsPoint(self.labelFive.frame, point))
+        {
+            return self.labelFive;
+        }
+    else
+        if (CGRectContainsPoint(self.labelSix.frame, point))
+        {
+            return self.labelSix;
+        }
+    else
+        if (CGRectContainsPoint(self.labelSeven.frame, point))
+        {
+            return self.labelSeven;
+        }
+    else
+        if (CGRectContainsPoint(self.labelEight.frame, point))
+        {
+            return self.labelEight;
+        }
+    else
+        if (CGRectContainsPoint(self.labelNine.frame, point))
+        {
+            return self.labelNine;
+        }
+    return nil;
 }
 
 - (IBAction)onLabelTapped:(UITapGestureRecognizer *)gesture
 {
-    //find labelone
     CGPoint point = [gesture locationInView:self.view];
-    if (CGRectContainsPoint(self.labelOne.frame, point)) {
-        self.labelOne.text = self.whichPlayerLabel.text;
-        self.labelOne.textColor = self.whichPlayerLabel.textColor;
+    UILabel *label = [self findLabelUsingPoint:point];
+    if (CGRectContainsPoint(label.frame, point))
+    {
+        if ([self.whichPlayerLabel.text isEqualToString: @"X"])
+        {
+            label.text = self.whichPlayerLabel.text;
+            label.textColor = self.whichPlayerLabel.textColor;
+            self.whichPlayerLabel.text = @"O";
+            self.whichPlayerLabel.textColor = [UIColor redColor];
+        }
+        else
+            if ([self.whichPlayerLabel.text isEqualToString: @"O"])
+            {
+                label.text = self.whichPlayerLabel.text;
+                label.textColor = self.whichPlayerLabel.textColor;
+                self.whichPlayerLabel.text = @"X";
+                self.whichPlayerLabel.textColor = [UIColor blueColor];
+            }
+
+        if ([label.text isEqualToString:@"O"] || [label.text isEqualToString:@"X"])
+        {
+
+        };
     }
-    //find labeltwo
-    else if (CGRectContainsPoint(self.labelTwo.frame, point)){
-        self.labelTwo.text = self.whichPlayerLabel.text;
-        self.labelTwo.textColor = self.whichPlayerLabel.textColor;
-
-    }
-    else if (CGRectContainsPoint(self.labelThree.frame, point)){
-        self.labelThree.text = self.whichPlayerLabel.text;
-        self.labelThree.textColor = self.whichPlayerLabel.textColor;
-        
-    }
-    else if (CGRectContainsPoint(self.labelFour.frame, point)){
-        self.labelFour.text = self.whichPlayerLabel.text;
-        self.labelFour.textColor = self.whichPlayerLabel.textColor;
-        
-    }
-    else if (CGRectContainsPoint(self.labelFive.frame, point)){
-        self.labelFive.text = self.whichPlayerLabel.text;
-        self.labelFive.textColor = self.whichPlayerLabel.textColor;
-        
-    }
-    else if (CGRectContainsPoint(self.labelSix.frame, point)){
-        self.labelSix.text = self.whichPlayerLabel.text;
-        self.labelSix.textColor = self.whichPlayerLabel.textColor;
-        
-    }
-    else if (CGRectContainsPoint(self.labelSeven.frame, point)){
-        self.labelSeven.text = self.whichPlayerLabel.text;
-        self.labelSeven.textColor = self.whichPlayerLabel.textColor;
-        
-    }
-    else if (CGRectContainsPoint(self.labelEight.frame, point)){
-        self.labelEight.text = self.whichPlayerLabel.text;
-        self.labelEight.textColor = self.whichPlayerLabel.textColor;
-        
-    }
-    else if (CGRectContainsPoint(self.labelNine.frame, point)){
-        self.labelNine.text = self.whichPlayerLabel.text;
-        self.labelNine.textColor = self.whichPlayerLabel.textColor;
-
-    }
-
-    if ([self.whichPlayerLabel.text isEqual: @"X"]) {
-        self.whichPlayerLabel.text = @"O";
-    }
-    else if([self.whichPlayerLabel.text isEqual: @"O"]) {
-        self.whichPlayerLabel.text = @"X";
-    }
-
-
-
-
-
-
-
-//
-//    CGPoint point = [gesture locationInView:self.view];
-//    self.theFutureLabel.center = point;
-//    if (CGRectContainsPoint(self.thePrecogsLabel.frame, point))
-//    {
-//        self.theFutureLabel.text = @"foobar foobar foobar";
-//        self.theFutureLabel.backgroundColor = [UIColor redColor];
-//        [self.theFutureLabel sizeToFit];
-//    }
-
-
 }
 
+
+
 @end
+
+
+
