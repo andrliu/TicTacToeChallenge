939d545773f0445910fdff71fe78e29ada621553
Andrew Liu <dieseliu321@gmail.com>
2014-10-25 17:15:27 -0700

    AI#1

--- TicTacToeChallenge/RootViewController.m
+++ TicTacToeChallenge/RootViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "RootViewController.h"
-#define kTime 10;
+#define kTime 2;
 
 @interface RootViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *labelOne;
@@ -57,6 +57,11 @@
     self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(countDown) userInfo:nil repeats:YES];
 }
 
+- (void)stopTime
+{
+    [self.timer invalidate];
+}
+
 - (void)countDown
 {
     if (self.timing > 0)
@@ -67,19 +72,16 @@
     else
     {
         [self stopTime];
-        [self timeOut];
+//        [self playerTimeOut];
+        [self playerTimeOutComputer];
         [self startTime];
     }
 }
 
-- (void)stopTime
+- (void)playerTimeOut
 {
-    [self.timer invalidate];
-}
-
-- (void)timeOut
-{
-    if ([self.whichPlayerLabel.text isEqualToString:@"Player"])
+    if ([self.whichPlayerLabel.text isEqualToString:@"Player"]||
+        [self.whichPlayerLabel.text isEqualToString:@"X"])
     {
         [self oTurn];
     }
@@ -88,22 +90,17 @@
         {
             [self xTurn];
         }
-        else
-            if ([self.whichPlayerLabel.text isEqualToString:@"X"])
-            {
-                [self oTurn];
-            }
 }
 
-- (void)computer
+
+- (void)playerTimeOutComputer
 {
-    if ([self.labelFive.text isEqualToString:@"X"]) {
-        <#statements#>
+    if ([self.whichPlayerLabel.text isEqualToString:@"Player"]||
+        [self.whichPlayerLabel.text isEqualToString:@"X"])
+    {
+        [self oTurn];
+        [self computerAI];
     }
-
-
-
-
 }
 
 - (UILabel *)findLabelUsingPoint:(CGPoint)point
@@ -117,41 +114,41 @@
         {
             return self.labelTwo;
         }
-    else
-        if (CGRectContainsPoint(self.labelThree.frame, point))
-        {
-            return self.labelThree;
-        }
-    else
-        if (CGRectContainsPoint(self.labelFour.frame, point))
-        {
-            return self.labelFour;
-        }
-    else
-        if (CGRectContainsPoint(self.labelFive.frame, point))
-        {
-            return self.labelFive;
-        }
-    else
-        if (CGRectContainsPoint(self.labelSix.frame, point))
-        {
-            return self.labelSix;
-        }
-    else
-        if (CGRectContainsPoint(self.labelSeven.frame, point))
-        {
-            return self.labelSeven;
-        }
-    else
-        if (CGRectContainsPoint(self.labelEight.frame, point))
-        {
-            return self.labelEight;
-        }
-    else
-        if (CGRectContainsPoint(self.labelNine.frame, point))
-        {
-            return self.labelNine;
-        }
+        else
+            if (CGRectContainsPoint(self.labelThree.frame, point))
+            {
+                return self.labelThree;
+            }
+            else
+                if (CGRectContainsPoint(self.labelFour.frame, point))
+                {
+                    return self.labelFour;
+                }
+                else
+                    if (CGRectContainsPoint(self.labelFive.frame, point))
+                    {
+                        return self.labelFive;
+                    }
+                    else
+                        if (CGRectContainsPoint(self.labelSix.frame, point))
+                        {
+                            return self.labelSix;
+                        }
+                        else
+                            if (CGRectContainsPoint(self.labelSeven.frame, point))
+                            {
+                                return self.labelSeven;
+                            }
+                            else
+                                if (CGRectContainsPoint(self.labelEight.frame, point))
+                                {
+                                    return self.labelEight;
+                                }
+                                else
+                                    if (CGRectContainsPoint(self.labelNine.frame, point))
+                                    {
+                                        return self.labelNine;
+                                    }
     return nil;
 }
 
@@ -168,56 +165,84 @@
         {
             return self.labelOne.text;
         }
-    else
-        if ([self.labelOne.text isEqualToString: self.labelFour.text] &&
-            [self.labelOne.text isEqualToString: self.labelSeven.text ])
-        {
-            return self.labelOne.text;
-        }
-    else
-        if ([self.labelTwo.text isEqualToString: self.labelFive.text] &&
-            [self.labelTwo.text isEqualToString: self.labelEight.text])
-        {
-            return self.labelTwo.text;
-        }
-    else
-        if ([self.labelThree.text isEqualToString: self.labelFive.text] &&
-            [self.labelThree.text isEqualToString: self.labelSeven.text])
-        {
-            return self.labelThree.text;
-        }
-    else
-        if ([self.labelThree.text isEqualToString: self.labelSix.text] &&
-            [self.labelThree.text isEqualToString: self.labelNine.text])
-        {
-            return self.labelThree.text;
-        }
-    else
-        if ([self.labelFour.text isEqualToString: self.labelFive.text] &&
-            [self.labelFour.text isEqualToString: self.labelSix.text])
-        {
-            return self.labelFour.text;
-        }
-    else
-        if ([self.labelSeven.text isEqualToString: self.labelEight.text] &&
-            [self.labelSeven.text isEqualToString: self.labelNine.text])
-        {
-            return self.labelSeven.text;
-        }
-    else
-        if (![self.labelOne.text     isEqualToString: @"1"] &&
-            ![self.labelTwo.text     isEqualToString: @"2"] &&
-            ![self.labelThree.text   isEqualToString: @"3"] &&
-            ![self.labelFour.text    isEqualToString: @"4"] &&
-            ![self.labelFive.text    isEqualToString: @"5"] &&
-            ![self.labelSix.text     isEqualToString: @"6"] &&
-            ![self.labelSeven.text   isEqualToString: @"7"] &&
-            ![self.labelEight.text   isEqualToString: @"8"] &&
-            ![self.labelNine.text    isEqualToString: @"9"])
-        {
-            return @"draw";
-        }
+        else
+            if ([self.labelOne.text isEqualToString: self.labelFour.text] &&
+                [self.labelOne.text isEqualToString: self.labelSeven.text ])
+            {
+                return self.labelOne.text;
+            }
+            else
+                if ([self.labelTwo.text isEqualToString: self.labelFive.text] &&
+                    [self.labelTwo.text isEqualToString: self.labelEight.text])
+                {
+                    return self.labelTwo.text;
+                }
+                else
+                    if ([self.labelThree.text isEqualToString: self.labelFive.text] &&
+                        [self.labelThree.text isEqualToString: self.labelSeven.text])
+                    {
+                        return self.labelThree.text;
+                    }
+                    else
+                        if ([self.labelThree.text isEqualToString: self.labelSix.text] &&
+                            [self.labelThree.text isEqualToString: self.labelNine.text])
+                        {
+                            return self.labelThree.text;
+                        }
+                        else
+                            if ([self.labelFour.text isEqualToString: self.labelFive.text] &&
+                                [self.labelFour.text isEqualToString: self.labelSix.text])
+                            {
+                                return self.labelFour.text;
+                            }
+                            else
+                                if ([self.labelSeven.text isEqualToString: self.labelEight.text] &&
+                                    [self.labelSeven.text isEqualToString: self.labelNine.text])
+                                {
+                                    return self.labelSeven.text;
+                                }
+                                else
+                                    if (![self.labelOne.text     isEqualToString: @"1"] &&
+                                        ![self.labelTwo.text     isEqualToString: @"2"] &&
+                                        ![self.labelThree.text   isEqualToString: @"3"] &&
+                                        ![self.labelFour.text    isEqualToString: @"4"] &&
+                                        ![self.labelFive.text    isEqualToString: @"5"] &&
+                                        ![self.labelSix.text     isEqualToString: @"6"] &&
+                                        ![self.labelSeven.text   isEqualToString: @"7"] &&
+                                        ![self.labelEight.text   isEqualToString: @"8"] &&
+                                        ![self.labelNine.text    isEqualToString: @"9"])
+                                    {
+                                        return @"draw";
+                                    }
     return nil;
+}
+
+- (void)checkingWinnerX
+{
+    if ([[self whoWon] isEqualToString: @"X"] ||
+        [[self whoWon] isEqualToString: @"O"] ||
+        [[self whoWon] isEqualToString: @"draw"])
+    {
+        [self alert];
+    }
+    else
+    {
+        [self oTurn];
+    }
+}
+
+- (void)checkingWinnerO
+{
+    if ([[self whoWon] isEqualToString: @"X"] ||
+        [[self whoWon] isEqualToString: @"O"] ||
+        [[self whoWon] isEqualToString: @"draw"])
+    {
+        [self alert];
+    }
+    else
+    {
+        [self xTurn];
+    }
 }
 
 - (NSString *)resultMessage
@@ -234,6 +259,20 @@
             return [NSString stringWithFormat:@"GG"];
         }
     return nil;
+}
+
+- (void)alert
+{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"" message: [self resultMessage] preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *restartButton = [UIAlertAction actionWithTitle:@"Restart" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action)
+                                    {
+                                        [self resetLabel];
+                                        [self stopTime];
+                                        [self startTime];
+                                    }
+                                    ];
+    [alert addAction:restartButton];
+    [self presentViewController: alert animated:YES completion:nil];
 }
 
 - (void)resetLabel
@@ -261,33 +300,139 @@
     self.navigationItem.title       = [NSString stringWithFormat:@"Tic Tac Toe Challenge"];
 }
 
-- (void)alert
+-(void)winner158
 {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"" message: [self resultMessage] preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction *restartButton = [UIAlertAction actionWithTitle:@"Restart" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action)
-                                    {
-                                        [self resetLabel];
-                                        [self stopTime];
-                                        [self startTime];
-                                    }
-                                    ];
-    [alert addAction:restartButton];
-    [self presentViewController: alert animated:YES completion:nil];
+    if ([self.labelOne.text isEqualToString: self.labelTwo.text] &&
+        [self.labelOne.text isEqualToString: self.labelThree.text])
+    {
+        return self.labelOne.text;
+    }
+    else
+        if ([self.labelOne.text isEqualToString: self.labelFive.text] &&
+            [self.labelOne.text isEqualToString: self.labelNine.text])
+        {
+            return self.labelOne.text;
+        }
+        else
+            if ([self.labelOne.text isEqualToString: self.labelFour.text] &&
+                [self.labelOne.text isEqualToString: self.labelSeven.text ])
+            {
+                return self.labelOne.text;
+            }
+            else
+                if ([self.labelTwo.text isEqualToString: self.labelFive.text] &&
+                    [self.labelTwo.text isEqualToString: self.labelEight.text])
+                {
+                    return self.labelTwo.text;
+                }
+                else
+                    if ([self.labelThree.text isEqualToString: self.labelFive.text] &&
+                        [self.labelThree.text isEqualToString: self.labelSeven.text])
+                    {
+                        return self.labelThree.text;
+                    }
+                    else
+                        if ([self.labelThree.text isEqualToString: self.labelSix.text] &&
+                            [self.labelThree.text isEqualToString: self.labelNine.text])
+                        {
+                            return self.labelThree.text;
+                        }
+                        else
+                            if ([self.labelFour.text isEqualToString: self.labelFive.text] &&
+                                [self.labelFour.text isEqualToString: self.labelSix.text])
+                            {
+                                return self.labelFour.text;
+                            }
+                            else
+                                if ([self.labelSeven.text isEqualToString: self.labelEight.text] &&
+                                    [self.labelSeven.text isEqualToString: self.labelNine.text])
+                                {
+                                    return self.labelSeven.text;
+                                }
 }
 
-- (void) xTurn
+
+
+
+
+- (void)computerAI
+{
+    if ([self.labelFive.text isEqualToString:@"5"])
+    {
+        self.labelFive.text = @"O";
+        self.labelFive.textColor = [UIColor redColor];
+        [self checkingWinnerO];
+    }
+    else
+        if ([self.labelOne.text isEqualToString:@"1"])
+        {
+            self.labelOne.text = @"O";
+            self.labelOne.textColor = [UIColor redColor];
+            [self checkingWinnerO];
+        }
+        else
+            if ([self.labelNine.text isEqualToString:@"9"])
+            {
+                self.labelNine.text = @"O";
+                self.labelNine.textColor = [UIColor redColor];
+                [self checkingWinnerO];
+            }
+//            else
+//                if ([self.labelNine.text isEqualToString:@"9"])
+//                {
+//                    self.labelNine.text = @"O";
+//                    self.labelNine.textColor = [UIColor redColor];
+//                    [self checkingWinnerO];
+//                }
+//                else
+//                    if ([self.labelNine.text isEqualToString:@"9"])
+//                    {
+//                        self.labelNine.text = @"O";
+//                        self.labelNine.textColor = [UIColor redColor];
+//                        [self checkingWinnerO];
+//                    }
+//                    else
+//                        if ([self.labelNine.text isEqualToString:@"9"])
+//                        {
+//                            self.labelNine.text = @"O";
+//                            self.labelNine.textColor = [UIColor redColor];
+//                            [self checkingWinnerO];
+//                        }
+//                        else
+//                            if ([self.labelNine.text isEqualToString:@"9"])
+//                            {
+//                                self.labelNine.text = @"O";
+//                                self.labelNine.textColor = [UIColor redColor];
+//                                [self checkingWinnerO];
+//                            }
+//                            else
+//                                if ([self.labelNine.text isEqualToString:@"9"])
+//                                {
+//                                    self.labelNine.text = @"O";
+//                                    self.labelNine.textColor = [UIColor redColor];
+//                                    [self checkingWinnerO];
+//                                }
+}
+
+- (void)xTurn
 {
     self.whichPlayerLabel.text = @"X";
     self.whichPlayerLabel.textColor = [UIColor blueColor];
 }
 
-- (void) oTurn
+- (void)oTurn
 {
     self.whichPlayerLabel.text = @"O";
     self.whichPlayerLabel.textColor = [UIColor redColor];
 }
 
-- (void) switchPlayer
+- (void)mark
+{
+    self.label.text = self.whichPlayerLabel.text;
+    self.label.textColor = self.whichPlayerLabel.textColor;
+}
+
+- (void)switchPlayer
 {
     if ([self.label.text isEqualToString:@"X"] ||
         [self.label.text isEqualToString:@"O"])
@@ -301,34 +446,33 @@
         {
             self.label.text = @"X";
             self.label.textColor = [UIColor blueColor];
-            if ([[self whoWon] isEqualToString: @"X"] ||
-                [[self whoWon] isEqualToString: @"O"] ||
-                [[self whoWon] isEqualToString: @"draw"])
-            {
-                [self alert];
-            }
-            else
-            {
-                [self oTurn];
-            }
+            [self checkingWinnerX];
         }
         else
             if ([self.whichPlayerLabel.text isEqualToString: @"O"])
             {
-                self.label.text = self.whichPlayerLabel.text;
-                self.label.textColor = self.whichPlayerLabel.textColor;
-                if ([[self whoWon] isEqualToString: @"X"] ||
-                    [[self whoWon] isEqualToString: @"O"] ||
-                    [[self whoWon] isEqualToString: @"draw"])
-                {
-                    [self alert];
-                }
-                else
-                {
-                    [self xTurn];
-                }
+                [self mark];
+                [self checkingWinnerO];
             }
     }
+}
+
+- (void)switchCompupter
+{
+    if ([self.label.text isEqualToString:@"X"] ||
+        [self.label.text isEqualToString:@"O"])
+    {
+        nil;
+    }
+    else
+        if ([self.whichPlayerLabel.text isEqualToString: @"Player"] ||
+            [self.whichPlayerLabel.text isEqualToString: @"X"])
+        {
+            self.label.text = @"X";
+            self.label.textColor = [UIColor blueColor];
+            [self checkingWinnerX];
+            [self computerAI];
+        }
 }
 
 - (IBAction)onLabelTapped:(UITapGestureRecognizer *)gesture
@@ -337,13 +481,14 @@
     self.label = [self findLabelUsingPoint:self.touchPoint];
     if (CGRectContainsPoint(self.label.frame, self.touchPoint))
     {
-        [self switchPlayer];
         [self stopTime];
+//        [self switchPlayer];
+        [self switchCompupter];
         [self startTime];
     }
 }
 
-- (void) panGestureAnimation
+- (void)panGestureAnimation
 {
     [UIView animateWithDuration:0 animations:^
      {
@@ -362,8 +507,9 @@
         {
             if (CGRectContainsPoint(self.label.frame, self.touchPoint))
             {
-                [self switchPlayer];
                 [self stopTime];
+//                [self switchPlayer];
+                [self switchCompupter];
                 [self startTime];
             }
             [self panGestureAnimation];
